4d07630313c24a7f08fbc7d9945622165137d7b5
hyy <higherhan@163.com>
2023-05-25 08:55:27 +0800

    feat: init

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    'vue/no-duplicate-attributes': 'off',
+    'vue/valid-v-for': 'off',
+  },
+  ignorePatterns: ['*.html', 'dist/*'], // 忽略文件
+};
