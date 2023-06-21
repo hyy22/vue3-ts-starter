992d2cd841620b13626d0a26ad2ee5598bdd3aaa
hyy <higherhan@163.com>
2023-06-21 10:45:18 +0800

    feat: 添加默认值

--- template.config.mjs
+++ template.config.mjs
@@ -1,6 +1,11 @@
 export default {
   prompts: [
-    { type: 'input', name: 'name', message: '输入项目名称，如vue3-ts-starter' },
+    {
+      type: 'input',
+      name: 'name',
+      message: '输入项目名称，如vue3-ts-starter',
+      default: process.env.DEFAULT_PROJECT_NAME,
+    },
     { type: 'input', name: 'title', message: '输入项目标题，如后台管理系统' },
   ],
   files: ['package.json', 'index.html'],
