db721172d9d7592c3688ef4c06c740a2b589058e
hyy <higherhan@163.com>
2023-08-10 10:00:04 +0800

    fix: 🐛 permission directive

--- src/directives/permission.ts
+++ src/directives/permission.ts
@@ -1,11 +1,9 @@
 import type { DirectiveBinding } from 'vue';
 import { usePermissionStore } from '@/store/permission';
 
-export default {
-  mounted(el: HTMLElement, binding: DirectiveBinding<any>) {
-    const permissionStore = usePermissionStore();
-    if (!permissionStore.hasPermission(String(binding.value))) {
-      el.remove();
-    }
-  },
-};
+export default function (el: HTMLElement, binding: DirectiveBinding<any>) {
+  const permissionStore = usePermissionStore();
+  if (!permissionStore.hasPermission(String(binding.value))) {
+    el.remove();
+  }
+}
