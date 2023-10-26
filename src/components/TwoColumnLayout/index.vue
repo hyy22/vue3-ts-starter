bd4570d189ac88aef893a450718639fcb5a0bb3c
hyy <higherhan@163.com>
2023-10-26 16:43:47 +0800

    feat: 🎸 add layout

---
+++ src/components/TwoColumnLayout/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+interface Props {
+  base?: number;
+}
+const props = withDefaults(defineProps<Props>(), {
+  base: 300,
+});
+</script>
+
+<template>
+  <div class="flex h-full">
+    <div
+      class="p-5 overflow-y-auto flex-none border-r border-gray-100"
+      :style="{ width: `${props.base}px` }">
+      <slot name="left"></slot>
+    </div>
+    <div class="flex-auto min-w-0 overflow-y-auto">
+      <slot name="right"></slot>
+    </div>
+  </div>
+</template>
