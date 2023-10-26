e0a4ae8de774cdcde3e9c7719a1de197841082d3
hyy <higherhan@163.com>
2023-10-26 16:42:46 +0800

    feat: 🎸 add search hook

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,5 @@
 import { ElMessage } from 'element-plus';
+import { saveAs } from 'file-saver';
 import { type ResponseData } from './http';
 
 /**
@@ -12,11 +13,32 @@ export function isResponseOk<T = any>(resp: ResponseData<T>) {
  */
 export function handleResp<T = any>(
   resp: ResponseData<T>,
-  fn: (resp: ResponseData<T>['data']) => void
+  fn?: (resp: ResponseData<T>['data']) => void
 ) {
   if (isResponseOk(resp)) {
-    fn(resp.data);
+    if (typeof fn === 'function') fn(resp.data);
     return;
   }
   ElMessage.error(resp.msg);
+}
+/**
+ * 处理blob格式响应
+ * @param resp
+ * @param name
+ * @returns
+ */
+export async function handleBlobResponse(resp: Blob, name: string) {
+  if (resp instanceof Blob) {
+    if (resp.type === 'application/json') {
+      const text = await resp.text();
+      try {
+        const data = JSON.parse(text);
+        handleResp(data);
+      } catch {
+        ElMessage.error(text ?? '未知错误');
+      }
+      return;
+    }
+    saveAs(resp, name);
+  }
 }
---
+++ src/hooks/useFormSearch.ts
@@ -0,0 +1,106 @@
+import { type Ref, ref } from 'vue';
+import { ElMessage } from 'element-plus';
+import useResetModel from '@/hooks/useResetModel';
+import { handleBlobResponse } from '@/api';
+
+export interface FormatResp<T> {
+  list: T[];
+  total: number;
+}
+export default function useFormSearch<
+  T extends ObjectType = any,
+  U = any,
+  E = any
+>({
+  factory = () => ({} as T),
+  validateAndFormatParmas,
+  searchFn,
+}: {
+  // 工厂函数，用来处理查询model和数据重置
+  factory?: () => T;
+  // 查询数据校验格式化
+  validateAndFormatParmas?: (model: T) => Promise<U>;
+  // 查询方法
+  searchFn: (params?: U) => Promise<FormatResp<E>>;
+}) {
+  // 查询loading
+  const loading = ref(false);
+  const rows = ref<E[]>([]) as Ref<E[]>;
+  const page = ref<PageType>({
+    current: 1,
+    pageSize: 20,
+    total: 0,
+  });
+  const { model, resetModel } = useResetModel<T>(factory);
+  /**
+   * 查询
+   */
+  async function search(isResetPage = false) {
+    if (isResetPage) page.value.current = 1;
+    let params: U;
+    if (typeof validateAndFormatParmas === 'function') {
+      try {
+        params = await validateAndFormatParmas(model);
+      } catch (e: any) {
+        return ElMessage.error(e.message);
+      }
+    } else {
+      params = model as unknown as U;
+    }
+    try {
+      loading.value = true;
+      const { list = [], total = 0 } = await searchFn(params);
+      rows.value = list;
+      page.value.total = total;
+    } catch (e: any) {
+      ElMessage.error(e.message);
+    } finally {
+      loading.value = false;
+    }
+  }
+  /**
+   * 重置
+   */
+  function reset() {
+    resetModel();
+    search(true);
+  }
+  /**
+   * 导出
+   */
+  const exportLoading = ref(false);
+  async function exportFile(
+    exportFn: (params: U) => Promise<Blob>,
+    name: string
+  ) {
+    let params: U;
+    if (typeof validateAndFormatParmas === 'function') {
+      try {
+        params = await validateAndFormatParmas(model);
+      } catch (e: any) {
+        return ElMessage.error(e.message);
+      }
+    } else {
+      params = model as unknown as U;
+    }
+    exportLoading.value = true;
+    try {
+      const resp = await exportFn(params);
+      handleBlobResponse(resp, name);
+    } finally {
+      exportLoading.value = false;
+    }
+  }
+
+  return {
+    loading,
+    model,
+    page,
+    rows,
+    search,
+    reset,
+    exportLoading,
+    exportFile,
+    resetModel,
+  };
+}
