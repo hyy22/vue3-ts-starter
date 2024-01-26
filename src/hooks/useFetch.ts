6615554f4e98f646f304e854a0137ea5c935fa2c
hyy <higherhan@163.com>
2024-01-26 14:54:05 +0800

    fix: 🐛 修复useFetch

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -17,39 +17,41 @@ export function useFetch<T = any>(
   const loading = ref(false);
   // 是否全部加载完成
   const finished = ref(false);
+  // 是否加载出错
+  const error = ref(false);
   // 数据列表
   const dataList: Ref<T[]> = ref([]);
   // 加载方法
   async function load(isResetPage = false) {
-    // 如果正在加载中，直接返回
-    if (loading.value) return;
     // 重置页码查询
     if (isResetPage) {
       loading.value = false;
       finished.value = false;
+      error.value = false;
       page.value.current = 1;
       dataList.value = [];
     }
-    // 如果已经加载完毕，直接返回
-    if (finished.value) return;
     loading.value = true;
     try {
       const { list, total } = await fetchFn(page.value);
+      loading.value = false;
       dataList.value = [...dataList.value, ...list];
       // 页码+1
       page.value.current += 1;
       // 判断是否加载完成
-      if (total && dataList.value.length >= total) {
+      if (!total || dataList.value.length >= total) {
         finished.value = true;
       }
     } catch (e: any) {
+      loading.value = false;
+      error.value = true;
       showToast(e.message);
     }
-    loading.value = false;
   }
   return {
     loading,
     finished,
+    error,
     dataList,
     load,
   };
